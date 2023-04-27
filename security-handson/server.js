d9cdbb9bf7613d714e0d738aa679148b740c25ae
disneyLadySango <sangota.d.duck@gmail.com>
2023-04-27 09:12:36 +0900

    feat: 2.3.2 init server

---
+++ security-handson/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3050;
+
+app.get("/", (req, res, next) => {
+  res.end("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
